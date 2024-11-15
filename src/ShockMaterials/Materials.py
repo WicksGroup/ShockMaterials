2dbc7c1bbd5dcab07a79702df3e9114173cd7439
Pat LaChapelle <31046873+physicslifter@users.noreply.github.com>
2024-11-15 13:18:37 -0500

    Updating 11/15 mid day

--- src/ShockMaterials/Materials.py
+++ src/ShockMaterials/Materials.py
@@ -6,5 +6,14 @@ class Material:
     """
     General class for materials
     """
-    def __init__(self):
+    def __init__(self, ambient_density, unit_cell_shape):
+        """
+        ambient_density: float = the ambient density of the material in g/cm^3
+        """
+        self.ambient_density = ambient_density
+        self.unit_cell_shape = unit_cell_shape
+
+    def get_hugoniot(self):
         pass
+
+Diamond = Material(ambient_density = 3.43)
